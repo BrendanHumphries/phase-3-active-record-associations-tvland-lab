489194409775bbb9822a04361f72c001c7affbc4
Brendan Humphries <brendan.b.humphries@gmail.com>
2021-11-08 09:25:05 -0500

    commit tree 95879a70aec5f0ffa12e57cbf90c286f63c46bab

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,4 +1,14 @@
 class Actor < ActiveRecord::Base
     has_many :characters
     has_many :shows, through: :characters
+
+    def full_name
+        "#{first_name} #{last_name}"
+    end
+
+    def list_roles
+        roles = []
+        self.characters.each {|character| roles << "#{character.name} - #{character.show.name}"}
+        roles
+    end
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,4 +1,11 @@
 class Show < ActiveRecord::Base
     has_many :characters
     belongs_to :network
+    has_many :actors, through: :characters
+
+    def actors_list
+        actors_array = []
+        self.actors.each {|actor| actors_array << actor.full_name}
+        actors_array
+    end
 end
